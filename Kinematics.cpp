3619212d159b17261184605351a6079308ba7be0
zimengpan <zimengpan@gmail.com>
2016-11-14 23:03:32 -0500

    Update Kinematics.cpp

--- Kinematics.cpp
+++ Kinematics.cpp
@@ -1,65 +1,118 @@
 //
-//  Kinematics.cpp
-//  
+// Created by zimen on 11/13/2016.
 //
-//  Created by Pan, Zimeng on 11/12/16.
-//
-//
+#include <constrol.h>
+#include "LinearAlgebra.cpp"
+#include "main.cpp"
+struct PosLoc{
+    Vec P, A;
+    PosLoc(Vec P_, Vec A_) : P(P_), A(A_) {}
+};
 
-#include "Kinematics.hpp"
-#include "Seek.cpp"
-#include "LienarAlgebra.cpp"
-#include <cmath>
+struct Output{
+    Vec L, A;
+    Output(Vec L_, Vec A_) : L(L_), A(A_) {}
+};
 
-class Kinematic{
-    // Structure for all the kinematic data
-    Vec position, velocity;
-    std::double orientation, rotation;
-    
-    public:
-    void update(SteeringOuput &steering, std::double time);
+struct Force{
+    Vec;
+};
+
+class KinematicArrive{
+    Vec character, target, targetVelocity;
+    double const maxAcceleration = 5, maxSpeed = 80, targetRadius = 20,
+            slowRadius = 30, time2Target = 0.1;
+    double targetSpeed;
+public:
+    Output GetSteering(Vec target){
+        Output output = Output(0, 0);
+
+        double distance = target.dist(character);
+        if(distance < targetRadius){
+            // run hold-position
+        } else if (distance > slowRadius){
+            targetSpeed = maxSpeed;
+        } else {
+            targetSpeed = maxSpeed * distance / slowRadius;
+        }
+
+        targetVelocity = (target-character).norm()*targetSpeed;
+        output.L = targetVelocity - character.V;
+        output.L = output.L * (1/time2Target);
+
+        if(output.L.len() > maxAcceleration){
+            output.L = output.L.norm()*maxAcceleration;
+        }
+        return output;
+    }
+};
+
+void getNewOrientation(PosLoc pos){
+    pos.A =
 }
 
-struct SteeringOuput{
-    // Acceleration
-    Vec linear;
-    std::double angular;
+void getSteering(PosLoc &posloc) const
+{
+    // First work out the direction
+    posloc.P = target;
+    output->linear -= character->position;
+
+    // If there is no direction, do nothing
+    if (output->linear.squareMagnitude() > 0)
+    {
+        output->linear.normalise();
+        output->linear *= maxSpeed;
+    }
 }
 
-struct KinematicSteeringOutput{
-    // Structure for ouput
-    Vec velocity;
-    std::double rotation;
-}
-SteeringOutput steering;
+void KinematicFlee::getSteering(SteeringOutput* output) const
+{
+    // First work out the direction
+    output->linear = character->position;
+    output->linear -= *target;
 
-void Kinematic::update(SteeringOuput &steering, std::double time){
-    this.position += velocity * time;
-    this.orientation += rotation * time;
-    this.velocity += steering.linear * time;
-    this.rotation += steering.angular * time;
-}
-
-void getNewOrientation(double &currentOrientation, double &velocity){
-    // Note the oreintation is soley depends on its movement
-    if(velocity.length() > 0)
-        currentOrientation = atan2(-static.x, static.z);
+    // If there is no direction, do nothing
+    if (output->linear.squareMagnitude() > 0)
+    {
+        output->linear.normalise();
+        output->linear *= maxSpeed;
+    }
 }
 
-int main(){
-    while(1){
-        do{
-            // Standby
-        } while(signal == 0) {
-        
-            switch(key){
-                case 1:
-                    // Steering
-                case 2:
-                    // Circling
-                case 3:
-                    // Wandering
-            }
+void KinematicArrive::getSteering(SteeringOutput* output) const
+{
+    // First work out the direction
+    output->linear = *target;
+    output->linear -= character->position;
+
+    // If there is no direction, do nothing
+    if (output->linear.squareMagnitude() < radius*radius)
+    {
+        output->linear.clear();
+    }
+    else
+    {
+        // We'd like to arrive in timeToTarget seconds
+        output->linear *= ((real)1.0 / timeToTarget);
+
+        // If that is too fast, then clip the speed
+        if (output->linear.squareMagnitude() > maxSpeed*maxSpeed)
+        {
+            output->linear.normalise();
+            output->linear *= maxSpeed;
         }
     }
 }
+
+void KinematicWander::getSteering(SteeringOutput* output) const
+{
+    // Move forward in the current direction
+    output->linear = character->getOrientationAsVector();
+    output->linear *= maxSpeed;
+
+    // Turn a little
+    real change = randomBinomial();
+    output->angular = change * maxRotation;
+}
+
+}; // end of namespace
