44e02294c5a2328006f9bd63ef4acf313336196a
zimengpan <zimengpan@gmail.com>
2016-11-14 23:04:23 -0500

    Update LinearAlgebra.cpp

--- LinearAlgebra.cpp
+++ LinearAlgebra.cpp
@@ -1,39 +1,49 @@
 //
-//  LinearAlgebra.cpp
-//  
+// Created by zimen on 11/13/2016.
 //
-//  Created by Pan, Zimeng on 11/12/16.
-//
-//
-
-#include "LinearAlgebra.hpp"
-
+#include <cmath>
 struct Vec {
     double x, y, z;
-    Vec(double x_=0, double y_=0, double z_=0){
-        x=x_;
-        y=y_;
-        z=z_;
+
+    Vec(double x_ = 0, double y_ = 0, double z_ = 0) {
+        x = x_;
+        y = y_;
+        z = z_;
     }
-    Vec operator+(const Vec &b) const {
-        return Vec(x+b.x,y+b.y,z+b.z);
+
+    Vec operator+(const Vec &t) const {
+        return Vec(x + t.x, y + t.y, z + t.z);
     }
-    Vec operator-(const Vec &b) const {
-        return Vec(x-b.x,y-b.y,z-b.z);
+
+    Vec operator-(const Vec &t) const {
+        return Vec(x - t.x, y - t.y, z - t.z);
     }
+
     Vec operator*(double b) const {
-        return Vec(x*b,y*b,z*b);
+        return Vec(x * b, y * b, z * b);
     }
+
     Vec mult(const Vec &b) const {
-        return Vec(x*b.x,y*b.y,z*b.z);
+        return Vec(x * b.x, y * b.y, z * b.z);
     }
-    Vec& norm(){
-        return *this = *this * (1/sqrt(x*x+y*y+z*z));
+
+    Vec &norm() {
+        return *this = *this * (1 / sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2)));
     }
+
     double dot(const Vec &b) const {
-        return x*b.x+y*b.y+z*b.z;
-    } // cross:
-    Vec operator%(Vec&b){
-        return Vec(y*b.z-z*b.y,z*b.x-x*b.z,x*b.y-y*b.x);
+        return x * b.x + y * b.y + z * b.z;
+    }
+
+    double dist(const Vec &b) const {
+        return sqrt(pow(x - b.x, 2) + pow(y - b.y, 2) + pow(z - b.z, 2));
+    }
+
+    double len() {
+        return sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
+    }
+
+    Vec operator%(Vec &b) {
+        return Vec(y * b.z - z * b.y, z * b.x - x * b.z, x * b.y - y * b.x);
     }
 };
